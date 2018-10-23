8dd757adb1511524b18c19038ed73973aebd8eb8
Christoph Kepper <christoph.kepper@pediapress.com>
2018-10-23 19:33:24 +0200

    add spacemacs personal-config

---
+++ personal-config/config.el
@@ -0,0 +1,15 @@
+;;##################################
+;; ORG-Mode
+;;##################################
+(setq org-startup-indented t)
+
+;;##################################
+;; DEFT
+;;##################################
+;; Dateien nicht in ~/.deft/ sondern in einen Unterverzeichnis des Org mode
+;; Verzeichnisses ablegen So werden die Notizdateien mit in mein Git-Repository
+;; einbezogen, in dem sich auch die anderen Org mode Dateien befinden.
+(setq deft-directory "~/Dropbox/org/notes/")
+
+;; Org-Mode statt Text verwenden
+(setq deft-default-extension "org")
---
+++ personal-config/keybindings.el
@@ -0,0 +1,2 @@
+;; Auf dem Laptop rufe ich Deft mit 'C-c n' auf.
+(global-set-key (kbd "C-c n") 'deft)
